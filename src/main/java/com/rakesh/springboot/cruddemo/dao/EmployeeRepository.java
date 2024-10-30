cf42996e6e8c7037c173c434c1e552e0f643cc33
RakeshkumarThera <rakeshkumarthera@gmail.com>
2024-10-29 20:36:34 -0400

    Customization, Pagination and Sorting

--- src/main/java/com/rakesh/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/rakesh/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.rakesh.springboot.cruddemo.dao;
 
 import com.rakesh.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> { //<Enity and Primary Key>
 }
